65f9ce2274a90e7e704892a075ac89ff1170e16d
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-04-15 12:43:53 -0300

    fix ground

--- apps/game-front/src/app/components/ground.tsx
+++ apps/game-front/src/app/components/ground.tsx
@@ -4,7 +4,7 @@ export function Ground() {
   return (
     <>
       <RigidBody type="fixed" colliders="trimesh">
-        <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+        <mesh position={[0, 0, 20]} rotation-x={-Math.PI / 2}>
           <planeGeometry args={[100, 100]} />
           <meshBasicMaterial color="#343434" />
         </mesh>
